00524c02b97146b7ec6ddb02448ae8f9b86d8626
KS <cherntay.s@gmail.com>
2021-09-17 22:21:26 +0700

    add code
    add some basic code and library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
 #include <Arduino.h>
+#include "esp_wifi.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Your ssid";
+const char* password = "password";
+const char* mqtt_server = "your ip";
+const char* ca_cert = "";
+
+#define mqtt_port 8883
+
+float humidity
+float temperature
+float heatIndex
+bool sound;
+bool light_status
+float fan_speed
+float servo_angle
 
 void setup() {
   // put your setup code here, to run once:
